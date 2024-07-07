b53e5f507a1655bcea0eaa7ecff032ee3835a51b
David <defaultik.personal@gmail.com>
2024-07-07 16:44:30 +0300

    send_message func; code optimization; check improvement

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,6 @@
 import discord
 import telegram_bot
-from tokens import DISCORD_TOKEN as TOKEN
+from tokens import DS_TOKEN, DS_CHAT_ID
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,30 +9,44 @@ bot = discord.Client(intents=intents)
 
 
 @bot.event
-async def on_ready():
-    print(f'[LOGS] Bot started to work')
+async def on_ready():    
+    print('[LOGS] %s started to work' % bot.user.name)
 
 
 @bot.event
 async def on_message(message):
-    if message.channel.name == "💬┊chat":
+    if message.author == bot.user:
+        return
+    
+    if message.channel.id == DS_CHAT_ID:
         if message.type == discord.MessageType.default:
-            if message.attachments:
-                print(f"{message.author.display_name}: {message.attachments[0].filename}")
-                await telegram_bot.send_attachment(f"*{message.author.display_name}:* [{message.attachments[0].filename}]({message.attachments[0].url})")
-            elif message.content:
-                print(f"{message.author.display_name}: {message.content}")
-                await telegram_bot.send_message(f"*{message.author.display_name}:* {message.content}")
+            if message.content:
+                await telegram_bot.send_message(message.author.display_name, message.content)
+            elif message.attachments:
+                await telegram_bot.send_attachment(message.author.display_name, message.attachments[0].filename, message.attachments[0].url)
 
         elif message.type == discord.MessageType.reply:
-            replied_message = (await message.channel.fetch_message(message.reference.message_id))
+            quote_msg = (await message.channel.fetch_message(message.reference.message_id))
             
-            if replied_message.attachments:
-                # print(f"{message.author.display_name}: {message.attachments[0].filename}")
-                await telegram_bot.send_message_advanced(f"<blockquote><b>{replied_message.author.display_name}:</b> {replied_message.attachments[0].filename}</blockquote><b>{message.author.display_name}:</b> {message.content}")
-            elif replied_message.content:
-                # print(f"{message.author.display_name}: {message.content}")
-                await telegram_bot.send_message_advanced(f"<blockquote><b>{replied_message.author.display_name}:</b> {replied_message.content}</blockquote><b>{message.author.display_name}:</b> {message.content}")
+            if quote_msg.content:
+                await telegram_bot.send_answer(quote_msg.author.display_name, quote_msg.content, message.author.display_name, message.content)
+            elif quote_msg.attachments:
+                await telegram_bot.send_answer(quote_msg.author.display_name, quote_msg.attachments[0].filename, message.author.display_name, message.content)
 
 
-bot.run(TOKEN)
+async def send_message(message):
+    await bot.wait_until_ready()
+
+    channel = bot.get_channel(DS_CHAT_ID)
+    if channel:
+        await channel.send(message)
+    else:
+        print(f"ERROR: Channel with ID {DS_CHAT_ID} not found")
+
+
+def run_discord_bot():
+    bot.run(DS_TOKEN)
+
+
+if __name__ == "__main__":
+    run_discord_bot()
