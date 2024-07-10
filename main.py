251632ac8a18e6b929545cd4c4a56fcd2df0fb16
David <defaultik.personal@gmail.com>
2024-07-10 20:07:50 +0300

    right formatting

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import asyncio
 import telegram_bot
 import discord_bot
 
-loop = asyncio.get_event_loop()
 
-loop.create_task(telegram_bot.main())
-loop.create_task(discord_bot.main())
+def main():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
-loop.run_forever()
+    loop.create_task(discord_bot.main())
+    loop.create_task(telegram_bot.main())
+
+    loop.run_forever()
+
+
+if __name__ == "__main__":
+    main()
