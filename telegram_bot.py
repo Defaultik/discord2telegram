2d1aac247c9d8188fafa9ce89df8deb1bbf56657
David <defaultik.personal@gmail.com>
2024-07-16 20:07:51 +0300

    code readability improved; unused library removed; comme

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,3 @@
-import asyncio
 import discord_bot
 from aiogram import Bot, Dispatcher, types
 from tokens import TG_TOKEN, TG_CHAT_ID
@@ -7,19 +6,25 @@ dp = Dispatcher()
 bot = Bot(token=TG_TOKEN)
 
 
-async def on_startup():
-    print("[TELEGRAM] Telegram Bot started to work")
-
-
+# Telegram message handler
 @dp.message()
 async def message_handler(message: types.Message):
     if message.chat.id == TG_CHAT_ID:
         if message.reply_to_message:
-            await discord_bot.send_answer(message.reply_to_message.from_user.username, message.reply_to_message.text, message.from_user.username, message.text)
+            await discord_bot.send_answer(
+                quote_author=message.reply_to_message.from_user.username, 
+                quote_text=message.reply_to_message.text, 
+                reply_author=message.from_user.username, 
+                reply_text=message.text
+            )
         else:
-            await discord_bot.send_message(message.from_user.username, message.text)
+            await discord_bot.send_message(
+                author=message.from_user.username, 
+                text=message.text
+            )
 
 
+# Send message to Telegram methods
 def escape_text(text):
     escape_characters = ("_", "*", "[", "]", "(", ")", "~", "`", ">", "#", "+", "-", "=", "|", "{", "}", ".", "!")
 
@@ -31,24 +36,39 @@ def escape_text(text):
 
 async def send_message(author: str, message: str) -> None:
     print(f"[DISCORD2TELEGRAM] {author}: {message}")
-    await bot.send_message(chat_id=TG_CHAT_ID, text=f"*{author}:* {message}", parse_mode="Markdown")
+    await bot.send_message(
+        chat_id=TG_CHAT_ID,
+        text=f"*{author}:* {message}",
+        parse_mode="Markdown"
+    )
 
 
 async def send_answer(quote_author: str, quote_text: str, reply_author: str, reply_text: str) -> None:
     print(f"[DISCORD2TELEGRAM] >{quote_author}: {quote_text}\n*{reply_author}: {reply_text}")
-    quote_author, quote_text, reply_author, reply_text = escape_text(quote_author), escape_text(quote_text), escape_text(reply_author), escape_text(reply_text)
-    await bot.send_message(chat_id=TG_CHAT_ID, text=f">*{quote_author}:* {quote_text}\n*{reply_author}:* {reply_text}", disable_web_page_preview=True, parse_mode="MarkdownV2")
+    
+    quote_author, quote_text, reply_author, reply_text = map(escape_text, [quote_author, quote_text, reply_author, reply_text])
+    await bot.send_message(
+        chat_id=TG_CHAT_ID,
+        text=f">*{quote_author}:* {quote_text}\n*{reply_author}:* {reply_text}", 
+        disable_web_page_preview=True, 
+        parse_mode="MarkdownV2"
+    )
 
 
 async def send_attachment(author: str, file_name: str, link: str) -> None:
     print(f"[DISCORD2TELEGRAM] {author}: {file_name} (attachment)")
-    await bot.send_message(chat_id=TG_CHAT_ID, text=f"*{author}:* [{file_name}]({link})", parse_mode="Markdown")
+    await bot.send_message(
+        chat_id=TG_CHAT_ID, 
+        text=f"*{author}:* [{file_name}]({link})",
+        parse_mode="Markdown"
+    )
+
+
+# Bot startup function
+async def on_startup():
+    print("[INFO] Telegram Bot started to work")
 
 
 async def main():
     dp.startup.register(on_startup)
     await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
